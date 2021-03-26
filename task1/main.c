9540914f10eafc7e5a045775e4ef8273f4550a3e
Kfir Cohen <cokfi@post.bgu.ac.il>
2021-03-26 14:22:35 +0300

    task comment and min max

--- task1/main.c
+++ task1/main.c
@@ -3,9 +3,31 @@
 //#---include <iostream> //for c++
 #include <stdio.h>
 #include <stdlib.h>
+// this code is emplementing three tasks:
+// Task 1 
+// input: array legnth(n) ,array of n integers.
+// the program finds it's Max Pooling with stride=1 and size = 1:n (result of n arrays)
+// output: sum of minimun numbers, (each max pooling result return array, each array return a minimum number)  
+int find_maximum(int a[], int n)
+{
+    int c, index = 0;
 
+    for (c = 1; c < n; c++)
+        if (a[c] > a[index])
+            index = c;
 
+    return index;
+}
+int find_minimum(int a[], int n) 
+{
+    int c, index = 0;
 
+    for (c = 1; c < n; c++)
+        if (a[c] < a[index])
+            index = c;
+
+    return index;
+}
 void question_1()
 {
     int ans = 1, n;
@@ -15,6 +37,7 @@ void question_1()
     for (int i = 0; i < n; i++)
         scanf_s("%d", &arr[i]);
     ///////  Enter solution here ///////
+    for (int i=0; i<n;i++)
 
     ////////////////////////////////////
     printf("%d\n", ans);
@@ -48,9 +71,11 @@ void question_3()
 }
 
 
+
 int main(int argc, char** argv)
 {
     int question;
+    printf("enter question number\n");
     scanf_s("%d", &question);
     if (question == 1)
         question_1();
