9d1cef9b718252535e4d9cf298383dd5763b8ce1
Kfir Cohen <cokfi@post.bgu.ac.il>
2021-04-02 15:41:32 +0300

    merged main with problem 2 , task1&2 complete

--- task1/main.c
+++ task1/main.c
@@ -10,8 +10,8 @@
 // input: array legnth - n ,array of n integers.
 // the program finds it's Max Pooling with stride=1 and size = 1:n (result of n arrays)
 // output: sum of minimun numbers, (each max pooling result return array, each array return a minimum number)
-//Task 2:
-//The sum of all jobs performed reduced by the minimum job existed when removed from queue
+// Task 2:
+// The sum of all jobs performed reduced by the minimum job existed when removed from queue
 // Task 3:
 
 int find_maximum(int a[], int n, int s)// s = the SubArray starting index , n = SubArray ending index+1, O(n-s)
