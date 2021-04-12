0d102d7448738e30c1d098d5b31ea76ba80c337f
Kfir Cohen <cokfi@post.bgu.ac.il>
2021-04-12 17:12:46 +0300

    fix problem 1

--- task1/main.c
+++ task1/main.c
@@ -35,42 +35,88 @@ int find_minimum(int a[], int n, int s)// s = the SubArray starting index , n = 
 
     return index;
 }
-void question_1()
-{
-    int ans = 0, n,minimum_of_maxP,temp_index,global_max_index;
+void question_1() {
+    /* 3 steps algorithm:
+    step1: Find indexes of next greater and previous greater for every element.
+    step2: Find the smallest element for each filter size and save them in array(that won't fill the entire array).
+    step 3: fill untouched filters and print the sum of step2's array.
+    */
+    int ans = 0, n, minimum_of_maxP, i;
     int* arr;
+    int* greater_right; // each cell contain the index of the greater element on right, =n if non exist
+    int* greater_left; // each cell contain the index of the greater element on left, =-1 if non exist
     scanf("%d", &n);
     arr = (int*)malloc(sizeof(int) * n);
+    greater_right = (int*)malloc(sizeof(int) * (n + 1));
+    greater_left = (int*)malloc(sizeof(int) * (n + 1));
+
     for (int i = 0; i < n; i++)
-       scanf("%d", &arr[i]);
-    //for (int i = 0; i < n; i++)
-    //    arr[i] = 1000;
-    //program time settings
+        scanf("%d", &arr[i]);
     clock_t start, end;
     double time_taken;
     start = clock();
-    //
-    // i+1= filter size, j= filtered_arr index, s= starting SubArray index
-    int* filtered_arr;
-    global_max_index = find_minimum(arr, n, 0);
-    ans = arr[global_max_index];// filter size = 1
-    for (int i = 1; i < n; i++) {
-        //filtered_arr = (int*)malloc(sizeof(int) * (n - i));
-        minimum_of_maxP = arr[find_maximum(arr, i + 1, 0)];// Max Pooling
-        for (int s = 1; s < (n - i); s++) {
-            temp_index = find_maximum(arr, s + i + 1, s);// Max Pooling
-            if (arr[temp_index] < minimum_of_maxP)
-                minimum_of_maxP = arr[temp_index];
+// step 1:
+    for (i = n - 1; i >= 0; i--) {
+        greater_left[i] = -1; // init values- no greater on right
+        greater_right[i] = n; // init values- no greater on left
+    }
+    int* index_array; // former greater element indexes array
+    index_array = (int*)malloc(sizeof(int) * (n + 1));
+    for (int i = 0; i <= n; i++)
+        index_array[i] = 0; // cell 0 is unused
+    int counter = 0; // index_array counter of current element 
+    for (i = 0; i < n; i++)// left elements
+    {
+        while (counter != 0 && arr[index_array[counter]] <= arr[i]) {
+            counter = counter - 1;
         }
-        ans = ans + minimum_of_maxP;
+
+        if (counter != 0)
+            greater_left[i] = index_array[counter];
+
+        counter += 1;
+        index_array[counter] = i;
+    }
+    counter = 0; // reset counter for right elements
+    for (i = n - 1; i >= 0; i--) {
+        while (counter != 0 && arr[index_array[counter]] <= arr[i]) {
+            counter = counter - 1;
+        }
+
+        if (counter != 0)
+            greater_right[i] = index_array[counter];
+
+        counter += 1;
+        index_array[counter] = i;
+    }
+// step 2:
+    int* min_of_MaxPooling;
+    int filter_size;
+    min_of_MaxPooling = (int*)malloc(sizeof(int) * (n + 1));
+    for (int i = 0; i <= n; i++)
+        min_of_MaxPooling[i] = 1001;// a cell for each filter size , cell 0 is unused
+    for (int i = 0; i < n; i++) {
+        filter_size = greater_right[i] - greater_left[i] - 1; // biggest filter size that will return arr[i] 
+        if (min_of_MaxPooling[filter_size] > arr[i])
+            min_of_MaxPooling[filter_size] = arr[i]; // save the min of all max pooling of the same filter size
+    }
+// step 3:
+    for (int i = n - 1; i >= 1; i--) {
+        if (min_of_MaxPooling[i] > min_of_MaxPooling[i + 1])
+            min_of_MaxPooling[i] = min_of_MaxPooling[i + 1];//fill empty cells,smaller filter size will have smaller/equal value
+    }
+    for (int i = 1; i <= n; i++) {
+        ans = ans + min_of_MaxPooling[i];
     }
     printf("%d\n", ans);
     free(arr);
+    free(index_array);
+    free(greater_left);
+    free(greater_right);
     //measure time
     end = clock();
     time_taken = ((double)(end - start)) / CLOCKS_PER_SEC;
     printf("fun() took %f seconds to execute \n", time_taken);
-    //
 }
 
 
--- task1/test.c
+++ task1/test.c
@@ -4,53 +4,70 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+/*
 void question_1() {
-    int ans = 0, n, minimum_of_maxP, temp_index, global_max_index;
+    /* 3 steps algorithm:
+    step1: Find indexes of next greater and previous greater for every element. 
+    step2: Find the smallest element for each filter size and save them in array.  
+    step 3: fill untouched filters and sum step2's array.
+    */
+/*
+    int ans = 0, n, minimum_of_maxP, i;
     int* arr;
-    int* bigger_right;
-    int* bigger_left;
+    int* greater_right; // each cell contain the index of the greater element on right, =n if non exist
+    int* greater_left; // each cell contain the index of the greater element on left, =-1 if non exist
     scanf("%d", &n);
     arr = (int*)malloc(sizeof(int) * n);
-    bigger_right = (int*)malloc(sizeof(int) * (n + 1));
-    bigger_left = (int*)malloc(sizeof(int) * (n + 1));
+    greater_right = (int*)malloc(sizeof(int) * (n + 1));
+    greater_left = (int*)malloc(sizeof(int) * (n + 1));
 
-    //for (int i = 0; i < n; i++)
-    //    scanf("%d", &arr[i]);
     for (int i = 0; i < n; i++)
-        arr[i] = 1000;
-    //program time settings
+        scanf("%d", &arr[i]);
     clock_t start, end;
     double time_taken;
     start = clock();
-    //
 
-    int next, i, j;
-    for (i = 0; i < n; i++) { // O(n^2)
-        bigger_right[i] = n; // init values- no bigger on left
-        for (j = i + 1; j < n; j++) {
-            if (arr[i] < arr[j]) {
-                bigger_right[i] = j;
-                break;
-            }
-        }
+    for (i = n - 1; i >= 0; i--) {
+        greater_left[i] = -1; // init values- no greater on right
+        greater_right[i] = n; // init values- no greater on left
     }
-    for (i = n - 1; i >= 0; i--) { // O(n^2)
-        bigger_left[i] = -1; // init values- no bigger on right
-        for (j = i - 1; j >= 0; j--) {
-            if (arr[i] < arr[j]) {
-                bigger_right[i] = j;
-                break;
-            }
+    int* index_array; // former greater element indexes array
+    index_array = (int*)malloc(sizeof(int) * (n + 1));
+    for (int i = 0; i <= n; i++)
+        index_array[i] = 0; // cell 0 is unused
+    int counter = 0; // index_array counter of current element 
+    for (i = 0; i < n; i++)// left elements
+    {
+        while (counter != 0 && arr[index_array[counter]] <= arr[i]) {
+            counter = counter - 1;
         }
+
+        if (counter != 0)
+            greater_left[i] = index_array[counter];
+
+        counter += 1;
+        index_array[counter] = i;
     }
-    // Create and initialize answer array
+    counter = 0; // reset counter for right elements
+    for (i = n - 1; i >= 0; i--) {
+        while (counter != 0 && arr[index_array[counter]] <= arr[i]) {
+            counter = counter - 1;
+        }
+
+        if (counter != 0)
+            greater_right[i] = index_array[counter];
+
+        counter += 1;
+        index_array[counter] = i;
+    }
+ 
     int* min_of_MaxPooling;
     int filter_size;
     min_of_MaxPooling = (int*)malloc(sizeof(int) * (n + 1));
     for (int i = 0; i <= n; i++)
-        min_of_MaxPooling[i] = n;// a cell for each filter size , cell 0 is unused
+        min_of_MaxPooling[i] = 1001;// a cell for each filter size , cell 0 is unused
     for (int i = 0; i < n; i++) {
-        filter_size = bigger_right[i] - bigger_left[i] - 1; // biggest filter size that will return arr[i] 
+        filter_size = greater_right[i] - greater_left[i] - 1; // biggest filter size that will return arr[i] 
         if (min_of_MaxPooling[filter_size] > arr[i])
             min_of_MaxPooling[filter_size] = arr[i]; // save the min of all max pooling of the same filter size
     }
@@ -62,18 +79,14 @@ void question_1() {
         ans = ans + min_of_MaxPooling[i];
     }
     printf("%d\n", ans);
+    free(arr);
+    free(index_array);
+    free(greater_left);
+    free(greater_right);
+    //measure time
+    end = clock();
+    time_taken = ((double)(end - start)) / CLOCKS_PER_SEC;
+    printf("fun() took %f seconds to execute \n", time_taken);
+    //
 }
-/*
-int main(int argc, char** argv)
-{
-    int question;
-    printf("enter question number\n");
-    scanf("%d", &question);
-    if (question == 1)
-        question_1();
-    /*        else if (question == 2)
-                question_2();
-            else
-                question_3();*/
-/*    return 0;
-}*/
+*/
