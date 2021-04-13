605ca4c306f2fc5bfa3f34621d384c6f544e8475
Kfir Cohen <cokfi@post.bgu.ac.il>
2021-04-13 13:00:11 +0300

    problem 2&1 fixed

--- task1/main.c
+++ task1/main.c
@@ -125,39 +125,82 @@ void question_1() {
 
 
 void question_2()
-/* worst case cenerio = first n/2 upgrowing elements(modulo 100K) then n/2 zeros
-[a*n/2 (first for loop) ]*[b (assignments) +c (find minimum operations)] = O(n)*[b+c] = O(n) 
-considering the best case when the minimum value is on the last element of the array,
-the function find_minimum will take n operations but will be needed only once so in every case we will get O(n).
-######question_2 O notations = O(n)###### 
+/* worst case cenerio = [1, 100K, 100K, 0, 0, 0, 1, 100K, 100K, 0, 0, 0, 1 ...]
+[n (first for loop) ]*[b (assignments) +*k('while loop operations)] = O(n)*[b+k] = O(k*n) 
+(k is bounded to 100K and statistically will be much less) 
+######question_2 O notations = O(k*n), (in our task we can write O(n), because k Smaller in magnitude from n)###### 
 */
 {
 
     unsigned long long ans = 0;
     int n, q_index = 0, min_index=0;// q_index is the index for the next spot available in queue 
-    int removal_q_index = 0,current_job;// removal_q_index is the starting index of the current queue 
+    int removal_q_index = 0,current_job,min_value;// removal_q_index is the starting index of the current queue 
     scanf("%d", &n);
+    clock_t start, end;
+    double time_taken;
+    start = clock();
     int* queue;
+//    int* build_worstcase;
+    int* value_to_index;
+    value_to_index = (int*)malloc(sizeof(int) * (101000));
     queue = (int*)malloc(sizeof(int) * n);
+/*    build_worstcase = (int*)malloc(sizeof(int) * n);
+    for (int i = 0; i < (n / 6); i ++) {
+        build_worstcase[4*i] = 1;
+        build_worstcase[4*i + 1] = 100000;
+        build_worstcase[4 * i + 2] = 99999;
+        build_worstcase[4*i + 3] = 0;
+        build_worstcase[4 * i + 4] = 0;
+        build_worstcase[4 * i + 5] = 0;
+
+    }
+    for (int i = n % 6; i > 0; i--)
+        build_worstcase[n - i] = 0;*/
+/*    for (int i = 100000; i < 3000000; i++)
+        build_worstcase[i] = 100000;
+    for (int i = 3000000; i < n; i++)
+        build_worstcase[i] = 0;*/
+    for (int i = 0; i <= 100000; i++)//initialize value_to_index
+        value_to_index[i] = 0;
     for (int i = 0; i < n; i++) {
         scanf("%d", &current_job);
+        //current_job = build_worstcase[i];
         if (current_job > 0) { // add to queue
             queue[q_index] = current_job;
             if ( queue[q_index]<=queue[min_index]){
                 min_index = q_index;
             }
             q_index += 1;
+            value_to_index[current_job] = q_index;
         }
         else if ((current_job == 0) && (q_index>removal_q_index)){ // remove from queue
             ans += queue[removal_q_index] - queue[min_index];
+            if (value_to_index[queue[removal_q_index]] == removal_q_index)
+                value_to_index[queue[removal_q_index]] = 0;
             removal_q_index += 1;
             if (min_index <= (removal_q_index-1)) {
-                min_index = find_minimum(queue, q_index, removal_q_index);
+                min_value = queue[min_index];
+                while (value_to_index[min_value] == 0 &&q_index>removal_q_index) {
+                    min_value += 1;
+                }
+                if (q_index == removal_q_index) {
+                    min_index = q_index;
+                }
+                else{
+                    min_index = value_to_index[min_value];
+                }
             }
    
         }
     }
     printf("%llu\n", ans);
+    free(queue);
+    //free(build_worstcase);
+    free(value_to_index);
+    //measure time
+    end = clock();
+    time_taken = ((double)(end - start)) / CLOCKS_PER_SEC;
+    printf("fun() took %f seconds to execute \n", time_taken);
 }
 
 void question_3()
